9191f96a433a4a64da865bd887d5b90502badfee
capa2784 <capa2784@gmail.com>
2024-09-20 09:41:24 +0900

    Add - 프로젝트 초기 세팅 / [Utility] Macro, Convert Enum 함수 추가

--- Source/ITT/ITT.Build.cs
+++ Source/ITT/ITT.Build.cs
@@ -7,7 +7,12 @@ public class ITT : ModuleRules
 	public ITT(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+		
+		PrivatePCHHeaderFile = "ITT.h";
+    		
+    	PrivateIncludePaths.Add("ITT");
+    	PrivateIncludePaths.Add("ITT/Utility");
+    		
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
