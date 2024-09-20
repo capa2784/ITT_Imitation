d2546b9ddeab0ca9f105d412cf256bc9cb603ee0
capa2784 <capa2784@gmail.com>
2024-09-20 09:32:54 +0900

    Add - 프로젝트 생성

---
+++ Source/ITT/ITT.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ITT : ModuleRules
+{
+	public ITT(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
