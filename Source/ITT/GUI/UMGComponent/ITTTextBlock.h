388a6d9a07b9ba8a2aae20244c0c3c8bba6833fd
capa2784 <capa2784@gmail.com>
2024-09-26 23:44:24 +0900

    Add - [System] GameUserSetting 랩핑 / [GUI] ITTTextBlock String Index 적용 : InGame에서 적용

--- Source/ITT/GUI/UMGComponent/ITTTextBlock.h
+++ Source/ITT/GUI/UMGComponent/ITTTextBlock.h
@@ -15,4 +15,39 @@ class ITT_API UITTTextBlock : public UCommonTextBlock
 	GENERATED_BODY()
 
 public:
+	UITTTextBlock();
+
+	
+protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	virtual void SynchronizeProperties() override;
+
+	virtual void OnTextChanged() override;
+	virtual void OnFontChanged() override;
+
+	
+public:
+	// ==================== Getter / Setter ==================== //
+	// ========== String Index ========== //
+	UFUNCTION(Category=ITT, BlueprintCallable)
+	void SetStringIndex(int32 _StringIndex);
+
+	UFUNCTION(Category=ITT, BlueprintPure)
+	int32 GetStringIndex() const { return StringIndex; }
+	// ================================== //
+	// ========================================================= //
+
+	
+protected:
+	// ========== String Index ========== //
+	void OnChangeStringIndex();
+	// ================================== //
+
+	
+protected:
+	// ========== String Index ========== //
+    UPROPERTY(Category=ITT, EditAnywhere, BlueprintGetter=GetStringIndex, BlueprintSetter=SetStringIndex)
+	int32 StringIndex;
+	// ================================== //
 };
+//ToDo : Editor View에서 적용값을 볼 수 있도록 개발
